c3f1a0cb53a90c01adeeeb3981216c24b2ffe9ac
kwood <155027796+qkewq@users.noreply.github.com>
2025-04-19 12:52:46 -0700

    Create server.c

---
+++ server.c
@@ -0,0 +1,14 @@
+#include <socksfive.h>
+#include <stdint.h>
+#include <sys/socket.h>
+
+int main(){
+
+//listen on port
+//establish tcp connection
+//call socks_handshake()
+//establish outgiong connection
+//start passing data
+
+    return 0;
+}
