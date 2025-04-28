28d095e4b087dc89881f39dfce354a8592d8d404
kwood <155027796+qkewq@users.noreply.github.com>
2025-04-28 15:51:32 -0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -5,7 +5,7 @@ sock.connect(('127.0.0.1', 8080))
 
 method_request = b'\x05\x01\x00'
 #127.0.0.1, 8081
-socks_requeset = b'\x05\x01\x00\x01\x01\x00\x00\x7f\x91\x1f'
+socks_requeset = b'\x05\x01\x00\x01\x7f\x00\x00\x01\x1f\x91'
 
 print("connected")
 sock.sendall(method_request)
